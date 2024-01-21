b601c9803c5d83b27cf0135837c76cb89aa76845
Neven Cvetkovic <neven.cvetkovic@gmail.com>
2024-01-21 11:32:58 +0100

    Step 3 - Prepare for Spring Boot 3.0
    Updated Spring Security to version 5.8 to prepare for Spring Boot 3.0
    Updated SpringSecurity class to remove WebSecurityConfigureAdapter and add @Configuration
    Updated configure method to return a SecurityFilterChain bean
    Updated deprecated method http.authorizeRequests() with http.authorizeHttpRequests() using requestMatchers() and AntPathRequestMatcher(String) methods.
    Updated passwordEncoder to use static factory method with defaults for Spring Security 5.8

--- src/main/java/example/cashcard/SecurityConfig.java
+++ src/main/java/example/cashcard/SecurityConfig.java
@@ -1,36 +1,40 @@
 package example.cashcard;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@Configuration
+public class SecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.
-                authorizeRequests((authz) ->
-                        authz
-                                .antMatchers("/cashcards/**").hasRole("CARD-OWNER")
-                                .antMatchers("/h2-console/**").permitAll()
-                )
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .requestMatchers(new AntPathRequestMatcher("/cashcards/**")).hasRole("CARD-OWNER")
+                .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
+                .and()
                 .csrf().disable()
                 .httpBasic(withDefaults());
+        return http.build();
     }
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new Pbkdf2PasswordEncoder();
+        return Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_8();
     }
 
     @Bean
